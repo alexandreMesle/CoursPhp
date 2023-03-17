f6a2e805eb6dc7d739aae1774998f14b2e21334e
Alexandre Meslé <alexandre.mesle@gmail.com>
2023-03-17 15:08:15 +0100

    [Calculatrice] Mise en forme HTML

--- calculatrice/index.php
+++ calculatrice/index.php
@@ -3,44 +3,85 @@
   <head>
     <title>Calculatrice</title>
   </head>
+
   <body>
-  <form method="get" action="get">
+  <form method="get" action=".">
+
+<!--
+            Opérande 1
+-->
+
     <input name="operande_1"
       <?php
       if (isset($_GET['operande_1']))
         print('value="'.$_GET['operande_1'].'"');
-      ?>
-    >
+      ?>>
+
+<!--
+            Opérateur
+-->
+
     <select name="operateur">
-      <option value="+"
-        <?php if ($_GET['operateur'] == '+') print('selected'); ?>
-      >+</option>
-      <option value="-"
-        <?php if ($_GET['operateur'] == '-') print('selected'); ?>
-      >-</option>
-      <option value="*"
-        <?php if ($_GET['operateur'] == '*') print('selected'); ?>
-      >*</option>
-      <option value="/"
-        <?php if ($_GET['operateur'] == '/') print('selected'); ?>
-      >/</option>
+      <option value="plus"
+      <?php
+      if (isset($_GET['operateur'])&&$_GET['operateur'] == 'plus')
+          print('selected');
+      ?>>
+        +
+      </option>
+      <option value="moins"
+      <?php
+      if (isset($_GET['operateur'])&&$_GET['operateur'] == 'moins')
+          print('selected');
+      ?>>
+        -
+      </option>
+      <option value="mul"
+      <?php
+      if (isset($_GET['operateur'])&&$_GET['operateur'] == 'mul')
+          print('selected');
+      ?>>
+        *
+      </option>
+      <option value="div"
+      <?php
+      if (isset($_GET['operateur'])&&$_GET['operateur'] == 'div')
+          print('selected');
+      ?>>
+        /
+      </option>
     </select>
+
+<!--
+            Opérande 2
+-->
+
     <input name="operande_2"
       <?php
       if (isset($_GET['operande_2']))
         print('value="'.$_GET['operande_2'].'"');
-      ?>
-    >
-    <input type="submit" value="Go !">
+      ?>>
+
+<!--
+             Bouton
+-->
+
+      <input type="submit" value="Go !">
   </form>
-  <?php if (is_numeric($_GET['operande_1']) && is_numeric($_GET['operande_2']))
+
+<!--
+            Résultat
+-->
+
+  <?php if (isset($_GET['operande_1']) && is_numeric($_GET['operande_1'])
+    && isset($_GET['operande_2']) && is_numeric($_GET['operande_2']))
   {
     switch($_GET['operateur'])
     {
-      case '+' : print("= " . ($_GET['operande_1']+$_GET['operande_2'])); break;
-      case '-' : print("= " . ($_GET['operande_1']-$_GET['operande_2'])); break;
-      case '*' : print("= " . ($_GET['operande_1']*$_GET['operande_2'])); break;
-      case '/' : print("= " . ($_GET['operande_1']/$_GET['operande_2'])); break;
+      case 'plus' : print("= " . ($_GET['operande_1']+$_GET['operande_2'])); break;
+      case 'moins' : print("= " . ($_GET['operande_1']-$_GET['operande_2'])); break;
+      case 'mul' : print("= " . ($_GET['operande_1']*$_GET['operande_2'])); break;
+      case 'div' : print("= " . ($_GET['operande_1']/$_GET['operande_2'])); break;
     }
   } ?>
   </body>
